bacd2a4b18816b6219242cc17d96a582764f67da
Rizhuycihuy <rizki003131@gmail.com>
2026-03-10 21:06:06 +0700

    add  input berat badan dan tinggi badan

--- programmenghitungbmi.cpp
+++ programmenghitungbmi.cpp
@@ -16,3 +16,9 @@ string kondisiBMI(float bmi) {
         return "Obesitas";
 }
 
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
